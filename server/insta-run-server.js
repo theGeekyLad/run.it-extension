b7b04ef63739e64648946e4ef1e2c498ed43e42f
theGeekyLad <rahul.pillai03@gmail.com>
2021-11-18 00:10:09 +0530

    Updated server port, hidden pass

--- server/insta-run-server.js
+++ server/insta-run-server.js
@@ -8,20 +8,25 @@ const server = http.createServer((req, res) => {
     let reqBody = '';
     req.on('data', chunk => reqBody += chunk);
     req.on('end', () => {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        if (req.method === 'GET') {
+            res.end(JSON.stringify({ message: 'heartbeat' }));
+            return;
+        }
         let command = JSON.parse(reqBody).command;
         let originalCommand = command;
         if (command.startsWith('sudo') && command.length > 5)
             command = `echo ${sudoPass} | sudo -S -p "" ${command.substring(5)}`
-        console.log(`Running command "${command}" ...`);
+        console.log(`\nRunning command "${originalCommand}" ...`);
         exec(command, { cwd: process.env.HOME }, (error, stdout, stderr) => {
             let result = { command: originalCommand };
             if (stderr) result['output'] = stderr;
             else if (stdout) result['output'] = stdout;
             else result['output'] = error;
-            res.writeHead(200, { 'Content-Type': 'application/json' });
             res.end(JSON.stringify(result));
         });
     });
 });
 
-server.listen(8000);
+server.listen(7236);
+console.log('Run.it server running on port 7236.');
